56894665c49bc737f02c7a28d88607fa15427e92
kthabhashwinni@gmail.com <kthabhashwinni@gmail.com>
2024-12-07 12:04:02 +0530

    push

---
+++ CompareTwoNumbers.java
@@ -0,0 +1,25 @@
+public class CompareTwoNumbers
+{
+ public static void main (String[] args)
+ {
+   int num1 =124;
+   int num2 =234;
+ 
+ if(num1 >num2)
+  {
+    System.out.println(num1 +" is greater than " +num2);
+  }
+ else if(num1 < num2)
+   {
+    System.out.println(num1 +" is less than " + num2);
+   }
+ else
+   {
+    System.out.println(num1 +"is equal to " +num2);
+    }
+  }
+}
+  
+
+
+
